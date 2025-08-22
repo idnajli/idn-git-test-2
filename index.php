2367f5fd64132fa7100dfdddd38cbe168f169c06
idnajli <idnajli@gmail.com>
2025-08-23 02:19:35 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa maiores assumenda aliquam deserunt nisi, corrupti ipsum iure, esse quos quod reprehenderit exercitationem pariatur eum minus molestias dolorum voluptatum, neque dolores.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
